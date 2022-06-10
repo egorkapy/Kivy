1c55859a05ab5cf136c5a4b7377a4f29962c90fe
egorkapy <egorka.vorobiev@gmail.com>
2022-06-10 13:32:06 +0300

    Add score
    I've corrected this error

--- find_cat/find_cat.py
+++ find_cat/find_cat.py
@@ -1,6 +1,7 @@
 from random import randint
 from kivy.app import App
 from kivy.graphics.svg import Svg
+from kivy.properties import NumericProperty
 from kivy.uix.label import Label
 from kivy.uix.scatter import Scatter
 from kivy.uix.floatlayout import FloatLayout
@@ -10,14 +11,13 @@ from kivy.clock import Clock
 from kivy.uix.widget import Widget
 
 # White background for testing the app
-# Window.clearcolor = 0, 0, 0, 0
-Window.clearcolor = 1, 1, 1, 1
+Window.clearcolor = 0, 0, 0, 0
+# Window.clearcolor = 1, 1, 1, 1
 
 CAT_SIZE = 50
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 600
 Window.size = WINDOW_WIDTH, WINDOW_HEIGHT
-score = 0
 
 Builder.load_file('sprites.kv')
 
@@ -36,13 +36,13 @@ class Light(Scatter):
 
 
 class Cat(SvgWidget):
+    score = 0
     cats = list()
 
     def __init__(self, filename, light, score_label, **kwargs):
         super(Cat, self).__init__(filename, **kwargs)
 
         self.score_label = score_label
-        self.score = score
 
         Cat.cats.append(self)
 
@@ -56,11 +56,13 @@ class Cat(SvgWidget):
 
         del cats[cats.index(self)]
 
-        if self.collide_point(Window.mouse_pos[0], Window.mouse_pos[1]) or\
-                self.collide_widget(cats[0]) or self.collide_widget(cats[1]):
+        if self.collide_point(Window.mouse_pos[0], Window.mouse_pos[1]):
             self.center = randint(CAT_SIZE, WINDOW_WIDTH - CAT_SIZE), randint(CAT_SIZE, WINDOW_HEIGHT - CAT_SIZE)
-            self.score += 1
-            self.score_label.text = f'Score = {self.score}'
+            Cat.score += 1
+            self.score_label.text = f'Score = {str(Cat.score)}'
+
+        elif self.collide_widget(cats[0]) or self.collide_widget(cats[1]):
+            self.center = randint(CAT_SIZE, WINDOW_WIDTH - CAT_SIZE), randint(CAT_SIZE, WINDOW_HEIGHT - CAT_SIZE)
 
 
 class GameApp(App):
@@ -68,7 +70,7 @@ class GameApp(App):
     def build(self):
         self.layout = FloatLayout()
         self.light = Light()
-        self.score_label = Label(text=f'Score = {score}', pos=(-120, 240), color=(0, 1, 1, 1))
+        self.score_label = Label(text='Score = 0', pos=(-120, 240), color=(0, 1, 1, 1))
 
         self.layout.add_widget(self.light)
 
