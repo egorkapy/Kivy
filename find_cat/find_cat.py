653e5b331e8d96bffdfe5d2b21c16b5e02f8bf2e
egorkapy <egorka.vorobiev@gmail.com>
2022-06-11 14:47:40 +0300

    Add sublight

--- find_cat/find_cat.py
+++ find_cat/find_cat.py
@@ -10,8 +10,8 @@ from kivy.clock import Clock
 from kivy.uix.widget import Widget
 
 # White background for testing the app
-Window.clearcolor = 0, 0, 0, 0
-# Window.clearcolor = 1, 1, 1, 1
+# Window.clearcolor = 0, 0, 0, 0
+Window.clearcolor = 1, 1, 1, 1
 
 CAT_SIZE = 50
 WINDOW_WIDTH = 400
@@ -55,12 +55,11 @@ class Light(Scatter):
         self.center = self.sub_light.center
 
 
-# TODO
-class SubWidget(Widget):
+class SubLight(Widget):
     def __init__(self, **kwargs):
-        super(SubWidget, self).__init__(**kwargs)
+        super(SubLight, self).__init__(**kwargs)
 
-        self.canvas.clear()
+        # self.canvas.clear()
 
     def on_touch_move(self, touch):
         self.center_x, self.center_y = (touch.x, touch.y)
@@ -103,7 +102,7 @@ class GameApp(App):
 
     def build(self):
         self.layout = FloatLayout()
-        self.sub_light = SubWidget()
+        self.sub_light = SubLight()
         self.light = Light(self.sub_light)
         self.score_label = Label(text='Score = 0', pos=(-120, 240), color=(0, 1, 1, 1))
 
